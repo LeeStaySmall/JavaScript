97960ab24ad18f777256ddd63fd06cb32ce738b5
李鼎 <liding@icourt.cc>
2018-05-09 17:23:01 +0800

    解构数组 ObjectDestructuring.js

---
+++ ES6/ObjectDestructuring.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/*
+* 对象解构
+* */
+
+function breakObj() {
+    return {dessert: '🎂', tea: '🍵', fruit: '🍎'}
+}
+
+// 这里为了便于理解，变量定义为var1, var2, var3,  key 是对象的属性， value是要赋值的变量
+let {dessert: var1, tea: var2, fruit: var3} = breakObj()
+
+console.log(var1, var2, var3);
